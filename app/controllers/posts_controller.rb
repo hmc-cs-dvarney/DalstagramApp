e7760b4fd6445ea7feefb48929dab90a4b26fb79
Dalton <dvarney@thedaltonv.com>
2017-02-09 19:51:10 -0800

    Updated Dalstagram

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@posts = Post.find(params[:id])
+  end
+
+  def create
+  	@post = Post.new(permit_post)
+  	if @post.save
+  		flash[:success] = "Great Success!"
+  		redirect_to post_path(@post)
+  	else
+  		flash[:error] = @post.errors.full_messages
+  		rendirect_ro new_post_path
+  	end
+  end
+
+  private
+  	def permit_post
+  		params.require(:post).permit(:image,:description)
+  	end
+end
